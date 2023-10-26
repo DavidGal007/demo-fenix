6f5004e816e16e06e435673d85183f9ac2f3c900
David <david.gal007@hotmail.com>
2023-10-26 19:00:54 -0300

    version 2

--- src/components/Invest.tsx
+++ src/components/Invest.tsx
@@ -1,45 +1,36 @@
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import Card from './Card';
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay } from 'swiper/modules';
+
 
 export default () => {
+
   return (
     <Swiper
       spaceBetween={24}
-      slidesPerView={4}
+      modules={[Autoplay]}
       loop={true}
-      centeredSlides={true}
-      modules={[Autoplay, Pagination, Navigation]}
-      // autoplay={{
-      //   delay: 500,
-      //   disableOnInteraction: false,
-      // }}
-      pagination={{
-        clickable: true,
+      speed={8000}
+      freeMode={true}
+      slidesPerView={'auto'}
+      // allowSlideNext={false}
+      a11y={{
+        enabled: false
       }}
-      breakpoints={{
-        375: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 3,
-          spaceBetween: 24,
-        },
-        1024: {
-          slidesPerView: 4,
-          spaceBetween: 24,
-        },
+      allowTouchMove={false}
+      // allowSlidePrev={false}
+      autoplay={{
+        delay: 0.5,
 
+        disableOnInteraction: false,
       }}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
       className='slider-center'
     >
+      <SwiperSlide><Card /></SwiperSlide>
       <SwiperSlide><Card /></SwiperSlide>
       <SwiperSlide><Card /></SwiperSlide>
       <SwiperSlide><Card /></SwiperSlide>
